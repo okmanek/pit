897129cd471ba3b4879ba159f11009c406a3b027
okmanek <okmanek@onet.eu>
2018-06-28 22:28:56 +0200

    first version of CI

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
 #!/usr/bin/env python
+import sys
 
-print('in main.py')
+#print('in main.py')
+
+commands_all = { 'add' : 'pit_add',
+                 'log' : 'pit_log',
+                 'status' : 'pit_status' }
+
+
+
+def pit_add():
+	print '<code for pit add>'
+
+def pit_log():
+	print '<code for pit log>'
+
+def pit_status():
+	print '<code for pit status>'
+
+def execute_cmd(cmdToExecute):
+	#print( 'in execute!')
+	print( 'In execute_cmd();   command to execute: ' + cmdToExecute )
+	cmdToExecute += '()'
+	eval( cmdToExecute )
+
+# if invoked with app name only, do nothing
+if len( sys.argv ) == 1:
+	pass
+# if executed with 1 arg... (e.g.: 'pit add')
+elif len( sys.argv ) == 2:
+	#and this arg is known in commands list...
+	if( sys.argv[1] in commands_all ):
+		#print related text
+		execute_cmd( commands_all[sys.argv[1]] )
+	else:
+		print( 'no such command' )
--- test.py
+++ test.py
@@ -3,11 +3,15 @@ import src.algorithms.sha1sum
 import src.algorithms.sha256sum
 log = True
 
-print('in test')
+print('in test.py')
 
 def test_algorithms():
 	#testing sha1sum
 	assert (src.algorithms.sha1sum.string2sha1sum('') == 'da39a3ee5e6b4b0d3255bfef95601890afd80709')
 
+def test_commands():
+	cmdsToExecuteWithResults = { 'pit add' : '<code for pit add>' }
+	for cmd in cmdsToExecuteWithResults:
+		pass
 
-test_algorithms()
+test_commands()
